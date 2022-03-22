6ac26976317f74ad9787b067037a496f94ecc08c
Alejandro González Reyes <alexglzrys@gmail.com>
2022-03-21 18:13:54 -0600

    resetear estilos globales  de la aplicación

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Product } from './shared/interfaces/product';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'grocery-store';
+  title = 'Mandarina';
 }
